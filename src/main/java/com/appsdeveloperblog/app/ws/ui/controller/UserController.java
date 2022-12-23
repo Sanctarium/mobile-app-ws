fca0c65857a839b64d554a905ad7f452d82d8f37
Sanctarium <ikozurov@gmail.com>
2022-12-23 14:16:14 +0500

    19. Query params init lesson.

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -3,8 +3,15 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users") //http://localhost:8080/users
+@RequestMapping("/users") //http://localhost:8080/users
 public class UserController {
+
+    @GetMapping
+    public String getUser(@RequestParam(value = "page") int page,
+                          @RequestParam(value = "limit") int limit) {
+        return "get user was called with page = " + page + " and limit = " + limit;
+    }
+
     @GetMapping(path = "/{userId}")
     public String getUser(@PathVariable String userId) {
         return "get user was called with userId = " + userId;
